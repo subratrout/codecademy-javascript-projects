1ad6d487f04b12e93fabbe490bbd940bfae5d4e4
BadMomber <kerstenk8@gmail.com>
2016-01-16 00:27:59 +0100

    Update script.js

--- gameboard/script.js
+++ gameboard/script.js
@@ -1,16 +1,12 @@
 var main = function() {
-    $('.more-btn').click(function(){
-        $('.more-menu').toggle();
-    })
-
-    $('.share').click(function(){
-        $('.share-menu').toggle();
-    })
-
-    $('.notification').click(function(){
-        $(this).toggleClass('active');
-    })
-
+  $(".more-btn").click(function() {
+    $(".more-menu").toggle();
+  })
+  $(".share").click(function() {
+    $(this).next().toggle();
+  })
+  $(".notification").click(function() {
+    $(this).toggleClass("active");
+  })
 };
-
 $(document).ready(main);
